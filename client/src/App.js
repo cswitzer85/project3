27f4f59c5a992c2aa149c8016256188a914148ee
Cody Switzer <cswitzer85@gmail.com>
2020-03-30 21:30:12 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import "./App.css";
 import MainContentContainer from "./components/MainContainer";
 
-
 const App = () => <MainContentContainer />;
-        
-
 
 export default App;
